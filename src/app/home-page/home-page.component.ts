361bc08989512103741f6deea0b83a230273fe3a
GKaszewski <gabrielkaszewski@gmail.com>
2020-04-20 19:03:50 +0200

    Finished home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit{
 
-  constructor() { }
+  constructor(private router : Router, private title : Title) {}
 
   ngOnInit(): void {
+    this.title.setTitle("Gabriel Kaszewski - Home");
   }
-
+  
+  goToPage(pageName : string):void {
+    this.router.navigate([`${pageName}`]);
+  }
 }
