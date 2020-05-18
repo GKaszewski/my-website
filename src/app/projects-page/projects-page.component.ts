97d0b8b6329214d65aa27854c59767cc53e24b4b
GKaszewski <gabrielkaszewski@gmail.com>
2020-05-18 21:35:49 +0200

    redesigned projects page

--- src/app/projects-page/projects-page.component.ts
+++ src/app/projects-page/projects-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Project } from '../models/project.model';
+import { ApiService } from '../services/ApiService.service';
 
 @Component({
   selector: 'app-projects-page',
@@ -7,11 +9,17 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./projects-page.component.scss']
 })
 export class ProjectsPageComponent implements OnInit {
-
-  constructor(private title : Title) { }
+  projects : Project[];
+  constructor(private title : Title, private api : ApiService) { }
 
   ngOnInit(): void {
     this.title.setTitle('Gabriel Kaszewski - Projects');
+    this.title.setTitle("Gabriel Kaszewski - Home");
+    this.api.getProjects().subscribe((res) =>  {
+      this.projects = res;
+      console.log(res);
+      console.log(this.projects[0]);
+    });
   }
 
 }
