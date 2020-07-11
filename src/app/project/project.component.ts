3cac392af2d0c98bc8452bc4bc646d16c4f61536
GKaszewski <gabrielkaszewski@gmail.com>
2020-07-12 00:45:49 +0200

    Added category filter

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { ProjectModalComponent } from '../project-modal/project-modal.component';
+import { Project } from '../models/project.model';
 
 @Component({
   selector: 'app-project',
@@ -8,22 +7,9 @@ import { ProjectModalComponent } from '../project-modal/project-modal.component'
   styleUrls: ['./project.component.scss']
 })
 export class ProjectComponent {
-  @Input() title : string;
-  @Input() technology : string;
-  @Input() desc : string;
-  @Input() imgUrl : string;
-  @Input() longDescription : string;
-  @Input() githubUrl : string;
-  @Input() downloadUrl : string;
-  @Input() visitUrl : string;
+  @Input() data : Project;
 
-  constructor(public dialog : MatDialog) {}
-
-  openModal() {
-    const dialogRef = this.dialog.open(ProjectModalComponent, { data: {title : this.title, tech : this.technology, 
-      desc : this.desc, imgUrl : this.imgUrl, 
-      longDesc : this.longDescription, githubUrl:this.githubUrl, downloadUrl:this.downloadUrl, visitUrl:this.visitUrl}});
-  }
+  constructor() {}
 
   goToPage(url : string) {
     window.open(url);
--- src/app/projects-page/projects-page.component.ts
+++ src/app/projects-page/projects-page.component.ts
@@ -11,26 +11,78 @@ import { MatAccordion } from '@angular/material/expansion';
   styleUrls: ['./projects-page.component.scss']
 })
 export class ProjectsPageComponent implements OnInit, OnDestroy {
-  projects : Project[];
-  sub : Subscription;
-  @ViewChild(MatAccordion) accordion : MatAccordion;
-  constructor(private title : Title, private api : ApiService, private meta : Meta) { }
+  allProjects: Project[] = [];
+  webProjects: Project[] = [];
+  gameProjects: Project[] = [];
+  mobileProjects: Project[] = [];
+  desktopProjects: Project[] = [];
+
+  currentlyDisplayedProjects: Project[] = [];
+  currentCategory : string = "";
+
+  sub: Subscription;
+  @ViewChild(MatAccordion) accordion: MatAccordion;
+  constructor(private title: Title, private api: ApiService, private meta: Meta) { }
   
   ngOnDestroy(): void {
     this.sub.unsubscribe();
   }
 
+  changeDisplayedProjects(mode: string) {
+    switch (mode) {
+      case "All":
+        this.currentlyDisplayedProjects = this.allProjects;
+        this.currentCategory = "";
+        break;
+      case "Games":
+        this.currentlyDisplayedProjects = this.gameProjects;
+        this.currentCategory = "Games";
+        break;
+      case "Web":
+        this.currentlyDisplayedProjects = this.webProjects;
+        this.currentCategory = "Web";
+        break;
+      case "Desktop":
+        this.currentlyDisplayedProjects = this.desktopProjects;
+        this.currentCategory = "Desktop";
+        break;
+      case "Mobile":
+        this.currentlyDisplayedProjects = this.mobileProjects;
+        this.currentCategory = "Mobile";
+        break;
+    }
+  }
+
   ngOnInit(): void {
-    this.sub = this.api.getProjects().subscribe((res) =>  {
-      this.projects = res;
-      for (let i = 0; i < this.projects.length; i++) {
-        let project = this.projects[i];
+    this.sub = this.api.getProjects().subscribe((res) => {
+      this.allProjects = res;
+      this.currentlyDisplayedProjects = this.allProjects;
+      for (let i = 0; i < this.allProjects.length; i++) {
+        let project = this.allProjects[i];
         let data = res[i];
+        project.category = data.category;
         project.thumbnailUrl = data.thumbnailUrl;
+        this.addToCollection(data, project);
       }
     });
     this.title.setTitle('Gabriel Kaszewski - Projects');
-    this.meta.updateTag({name: 'description', content: 'My projects'});
+    this.meta.updateTag({ name: 'description', content: 'My projects' });
   }
 
+  private addToCollection(data: any, project: Project) {
+    switch (data.category) {
+      case "Web":
+        this.webProjects.push(project);
+        break;
+      case "Game":
+        this.gameProjects.push(project);
+        break;
+      case "Desktop":
+        this.desktopProjects.push(project);
+        break;
+      case "Mobile":
+        this.mobileProjects.push(project);
+        break;
+    }
+  }
 }
