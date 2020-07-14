ad5fc9ad7093ff322c491b581f0523791cc358ba
GKaszewski <gabrielkaszewski@gmail.com>
2020-07-14 02:23:50 +0200

    Added google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'website';
+  title = 'Gabriel Kaszewski Website';
+
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'UA-172636969-1', {
+            'page_path': event.urlAfterRedirects
+          });
+      }
+    });
+  }
 }
